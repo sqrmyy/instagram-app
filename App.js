769dd41d9fb09e9df75e6d08ee4c8cd450e068dc
sqrmyy <dn41.ra20adtdjw@gmail.com>
2020-10-24 20:32:22 +0900

    add pagenation

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
 //import screens
-import Single1 from './screens/Single1';
+// import Single1 from './screens/Single1';
 // import Single2 from './screens/Single2';
-// import Stack1 from './screens/Stack1';
-// import Stack2 from './screens/Stack2';
+import Stack1 from './screens/Stack1';
+import Stack2 from './screens/Stack2';
 // import Tab1 from './screens/Tab1';
 // import Tab2 from './screens/Tab2';
 
+//stack
+const Stack = createStackNavigator(
+  {
+    Stack1: { screen: Stack1 },
+    Stack2: { screen: Stack2 },
+  },
+  {
+    initialRouteName: 'Stack1',
+  }
+);
+
 class App extends React.Component {
   render() {
-    return <Single1 />;
+    //AppContainerでラップ
+    const Layout = createAppContainer(Stack);
+    return <Layout />;
   }
 }
 
--- screens/Stack1.js
+++ screens/Stack1.js
@@ -6,6 +6,10 @@ class Stack1 extends React.Component {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Stack1</Text>
+        <Button
+          title='GoTo Stack2'
+          onPress={() => this.props.navigation.navigate('Stack2')}
+        />
       </View>
     );
   }
